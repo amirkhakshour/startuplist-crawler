03a86d1c896957786bd97217ad845d40d22330e8
amir-khakshour <khakshour.amir@gmail.com>
2020-03-07 20:01:45 +0100

    changed agent and added delay + completed region pages scrapping

--- neo/neo/spiders/betalist.py
+++ neo/neo/spiders/betalist.py
@@ -7,4 +7,14 @@ class BetalistSpider(scrapy.Spider):
     start_urls = ['https://betalist.com/regions/']
 
     def parse(self, response):
+        for region in response.xpath("//a[contains(@class, 'tag--card')]"):
+            region_url = response.urljoin(region.xpath('./@href').extract().pop())
+            region_text = region.xpath('./text()').extract().pop().strip().lower()
+            yield scrapy.Request(
+                url=region_url,
+                callback=self.extract_region_page,
+                meta={'region': region_text}
+            )
+
+    def extract_region_page(self, response):
         pass
